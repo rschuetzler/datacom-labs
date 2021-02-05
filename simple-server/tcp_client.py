1b523bbccc46c0e301792ef56e21b942d43a1e08
Ryan Schuetzler <ryan@schuetzler.net>
2021-02-05 14:55:48 -0700

    Make changing messages easier

--- simple-server/tcp_client.py
+++ simple-server/tcp_client.py
@@ -2,10 +2,11 @@ import socket
 
 HOST = "basic.schuetzler.net"
 PORT = 9090
+message = "i am whispering"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"howdy doody")
+    s.sendall(message.encode("utf-8"))
     data = s.recv(1024)
 
 print(f"Received {repr(data)}")
--- simple-server/udp_client.py
+++ simple-server/udp_client.py
@@ -1,15 +1,15 @@
 import socket
 
 HOST, PORT = "basic.schuetzler.net", 9091
-data = "Testing 123"
+message = "NOW I AM SHOUTING"  # The UDP server will lowercase the message
 
 # SOCK_DGRAM is the socket type to use for UDP sockets
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # As you can see, there is no connect() call; UDP has no connections.
 # Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
+sock.sendto(bytes(message + "\n", "utf-8"), (HOST, PORT))
 received = str(sock.recv(1024), "utf-8")
 
-print("Sent:     {}".format(data))
+print("Sent:     {}".format(message))
 print("Received: {}".format(received))
